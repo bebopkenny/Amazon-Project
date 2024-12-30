5cadc6014447e5f7cfec1392d1961dad477a1d2c
kenny <kennygarcia15@csu.fullerton.edu>
2024-12-29 20:30:15 -0800

    Make add to cart button interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -46,13 +46,44 @@ products.forEach((product) => {
             Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart" 
+        data-product-id="${product.id}"> 
             Add to Cart
         </button>
         </div>
     `;
 });
-
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId; // getting the name of the product
+        
+        let matchingItem;
+
+        cart.forEach((item) => {
+            if (productId === item.productId) { // if the product name matches 
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.push({
+            productId: productId,
+            quantity: 1,
+        });
+        }
+    });
+});
+
+
+
+/*
+Syntax for a Data Attribute
+- is just an HTML attribute
+- have to start with "data-"
+- then give it any name
+data-product-name="${product.name}"
+*/
