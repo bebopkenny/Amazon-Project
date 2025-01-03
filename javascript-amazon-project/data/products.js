d27e7cfb93c4fad60f6c678cf4d2a471bf1867a9
kenny <kennygarcia15@csu.fullerton.edu>
2025-01-02 20:54:38 -0800

    Use inheritance and polymorphism to add size chart

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -32,26 +32,48 @@ class Product {
   getPrice() {
     return  `$${formatCurrency(this.priceCents)}`
 
+  } 
+  extraInfoHTML() {
+    return '';
   }
 }
+  // Inheritance = lets us reuse code between classes
+  // Add properties and methods that are more specific to clothing
+  class Clothing extends Product {
+    sizeChartLink;
 
-const product1 = new Product( {
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    constructor(productDetails) {
+      super(productDetails); // Calls the constructor of the parent class
+      this.sizeChartLink = productDetails.sizeChartLink;
+    }
+
+    extraInfoHTML() {
+      // super.extraInfoHTML();
+      return `
+        <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+        </a>
+      `;
+    }
+}
+const tshirt = new Clothing({
+  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+  name: "Adults Plain Cotton T-Shirt - 2 Pack",
   rating: {
     stars: 4.5,
-    count: 87
+    count: 56
   },
-  priceCents: 1090,
+  priceCents: 799,
   keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-},
-);
-console.log(product1);
+    "tshirts",
+    "apparel",
+    "mens"
+  ],
+  type: "clothing",
+  sizeChartLink: "images/clothing-size-chart.png"
+});
+
 
 // Transforming each of the products into a class
 export const products = [
@@ -714,7 +736,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
-
-console.log(products)
